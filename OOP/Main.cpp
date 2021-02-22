6fb9cd95b7186c2b3be63abd4f0112f47ea2f119
iRussianHero <irussianhero@gmail.com>
2021-02-22 11:40:46 +0300

    Add project files.

---
+++ OOP/Main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Person
+{
+	string firstName;
+	string lastName;
+	int age;
+	double height;
+}; 
+
+void printPerson(Person person)
+{
+	cout << "=== Информация о человеке ===" << endl;
+	cout << "Имя:\t" << person.firstName << endl;
+	cout << "Фамилия:\t" << person.lastName << endl;
+	cout << "Возраст:\t" << person.age << endl;
+	cout << "Рост:\t" << person.height << endl;
+	cout << "=============================" << endl;
+}
+int main()
+{
+	Person person_1, person_2;
+	person_1.firstName = "Roman";
+	person_1.lastName = "Yakushev";
+	person_1.age = 33;
+	person_1.height = 184;
+
+	person_2.firstName = "Anna";
+	person_2.age = 20;
+
+	printPerson(person_1);
+	printPerson(person_2);
+
+	return 0;
+}
